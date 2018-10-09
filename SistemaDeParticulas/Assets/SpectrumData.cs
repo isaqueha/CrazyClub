c363777d0391aaaf4d31391381ee4b244d56f932
I868346 <maiara.lange@sap.com>
2018-10-09 00:23:10 -0300

    Removing unnecessary code and refactoring

---
+++ SistemaDeParticulas/Assets/SpectrumData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SpectrumData : MonoBehaviour
+{
+    void Update() {
+        var spectrum = getSpectrumFromAudio();
+        changeGameObjectToNewPosition(spectrum);
+    }
+
+    float[] getSpectrumFromAudio() {
+        var spectrum = new float[64];
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        return spectrum;
+    }
+
+    void changeGameObjectToNewPosition(float[] spectrum) {
+        var newPosition = new Vector3(spectrum[0] * 50, spectrum[32] * 50, spectrum[63] * 50);
+        gameObject.transform.position = newPosition;
+    }
+}
